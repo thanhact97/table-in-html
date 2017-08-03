c0ef52ad382e3fb4abf219d99635c7c0adfd82bf
thanhcr <thanhact97@gmail.com>
2017-08-03 10:10:44 +0700

    Update folder structure

---
+++ php/object/object_basic.php
@@ -0,0 +1,14 @@
+<?php
+//Da php la chi co php,ko chua code html
+            class Person{
+                public $isAlive = true;
+                public $firstname;
+                public $lastname;
+                public $age;
+             
+            }
+                $teacher = new Person();
+                $student = new Person();
+                
+                echo $teacher->isAlive;
+        ?>
